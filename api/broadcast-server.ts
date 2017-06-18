2d9f6dffddf9f3ee6a832adcd35cf62bed8c829a
Ryan Petrich <rpetrich@gmail.com>
2017-06-18 18:48:45 -0400

    Update broadcast to extend Global's interface and to cleanup when unregistering a transaction

--- api/broadcast-server.ts
+++ api/broadcast-server.ts
@@ -1,16 +1,32 @@
+declare module NodeJS  {
+	interface Global {
+		observers: ((message: string) => void)[] | undefined;
+	}
+}
+
 namespace concurrence {
-	export function broadcast(text: string) {
-		const observers = (global as any).observers;
+	export function broadcast(message: string) {
+		const observers = global.observers;
 		if (observers) {
 			for (let i = 0; i < observers.length; i++) {
-				observers[i](text);
+				observers[i](message);
 			}
 		}
 	}
-	export function receive(callback: (value: string) => void): ConcurrenceTransaction {
+	export function receive(callback: (message: string) => void): ConcurrenceTransaction {
 		const transaction = concurrence.observeServerEventCallback<typeof callback>(callback, false);
-		const observers = (global as any).observers || ((global as any).observers = []);
-		observers.push((text: string) => transaction.send(text));
+		const observers = global.observers || (global.observers = []);
+		const dispatch = (message: string) => transaction.send(message);
+		observers.push(dispatch);
+		const close = transaction.close;
+		transaction.close = function() {
+			// Cleanup when unregistering
+			const index = observers.indexOf(dispatch);
+			if (index != -1) {
+				observers.splice(index, 1);
+			}
+			return close.apply(this, arguments);
+		};
 		return transaction;
 	}
 }
