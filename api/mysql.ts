a5670c30efb5b318f4d217cf6f3e3e94dff192db
Ryan Petrich <rpetrich@gmail.com>
2017-08-27 02:15:38 -0400

    Simplify mysql wrappers

--- api/mysql.ts
+++ api/mysql.ts
@@ -2,7 +2,7 @@ namespace concurrence {
 	export namespace mysql {
 		export type ExecuteResult = { records?: { [column: string] : any}[], insertId?: number, affectedRows?: number };
 		export function query(host: string, query: string, ...params: any[]) : Promise<{[column: string] : any}[]> {
-			return execute(host, query, ...params).then(result => {
+			return execute.apply(concurrence, Array.prototype.slice.call(arguments)).then((result: ExecuteResult) => {
 				if (result.records) {
 					return result.records;
 				}
@@ -10,7 +10,7 @@ namespace concurrence {
 			});
 		}
 		export function modify(host: string, sql: string, ...params: any[]) : Promise<{insertId?: number, affectedRows: number}> {
-			return execute(host, sql, ...params).then(result => {
+			return execute.apply(concurrence, Array.prototype.slice.call(arguments)).then((result: ExecuteResult) => {
 				if (typeof result.affectedRows == "number") {
 					let wrappedResult : {insertId?: number, affectedRows: number} = { affectedRows: result.affectedRows };
 					if (typeof result.insertId == "number") {
