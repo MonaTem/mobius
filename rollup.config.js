5e20ed1faa9751521217ac99769028d322e161dd
Ryan Petrich <rpetrich@gmail.com>
2017-09-23 18:43:19 -0400

    Rewrite for/in loops and Object.keys calls into deterministic versions that call Array.sort()

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@ import { types } from "babel-core";
 import { pureBabylon as pure } from "side-effects-safe";
 
 function stripRedact() {
-	const stripFunctionNameList = ["redact"];
 	return {
 		visitor: {
 			CallExpression(path) {
@@ -20,6 +19,56 @@ function stripRedact() {
 	};
 }
 
+function rewriteForInStatements() {
+	return {
+		visitor: {
+			// Rewrite for (... in ...) into the equivalent source that iterates in a well-defined order
+			ForInStatement: {
+				exit(path) {
+					const node = path.node;
+					const rightIdentifier = path.scope.generateUidIdentifier("right");
+					const keyIdentifier = path.scope.generateUidIdentifier("key");
+					const keysIdentifier = path.scope.generateUidIdentifier("keys");
+					const iIdentifier = path.scope.generateUidIdentifier("i");
+					const keysSubIExpression = types.memberExpression(keysIdentifier, iIdentifier, true);
+					const body = node.body.type == "BlockStatement" ? node.body.body.slice() : [];
+					if (node.left.type == "VariableDeclaration") {
+						body.unshift(types.variableDeclaration(node.left.kind, [types.variableDeclarator(node.left.declarations[0].id, keysSubIExpression)]));
+					} else {
+						body.unshift(types.expressionStatement(types.assignmentExpression("=", node.left, keysSubIExpression)));
+					}
+					path.replaceWith(types.blockStatement([
+						types.variableDeclaration("var", [
+							types.variableDeclarator(rightIdentifier, node.right),
+							types.variableDeclarator(keyIdentifier),
+							types.variableDeclarator(keysIdentifier, types.arrayExpression([])),
+							types.variableDeclarator(iIdentifier, types.numericLiteral(0)),
+						]),
+						types.forInStatement(keyIdentifier, rightIdentifier, types.expressionStatement(types.callExpression(types.memberExpression(keysIdentifier, types.identifier("push")), [keyIdentifier]))),
+						types.forStatement(types.callExpression(types.memberExpression(keysIdentifier, types.identifier("sort")), []), types.binaryExpression("<", iIdentifier, types.memberExpression(keysIdentifier, types.identifier("length"))), types.unaryExpression("++", iIdentifier), types.blockStatement(body)),
+					]));
+					path.addComment("leading", "Deterministic for (... in ...)");
+					path.skip();
+				}
+			},
+			// Rewrite Object.keys(...) into Object.keys(...).sort()
+			CallExpression: {
+				exit(path) {
+					const node = path.node;
+					const callee = node.callee;
+					if (callee.type == "MemberExpression" && callee.object.type == "Identifier" && callee.object.name == "Object") {
+						if (callee.property.type == "Identifier" && callee.property.name == "keys") {
+							path.replaceWith(types.callExpression(types.memberExpression(node, types.identifier("sort")), []));
+							path.addComment("leading", "Deterministic Object.keys(...)");
+							path.skip();
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 export default {
 	entry: "src/app.js",
 	dest: "public/client.js",
@@ -33,7 +82,7 @@ export default {
 		}),
 		babel({
 			babelrc: false,
-			plugins: [stripRedact()]
+			plugins: [stripRedact(), rewriteForInStatements()]
 		})
 	]
 };
