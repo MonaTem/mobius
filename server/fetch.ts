43a4295f667a46fb290ee359ba4e673db5ec927e
Ryan Petrich <rpetrich@gmail.com>
2017-09-23 23:30:55 -0400

    Use async/await in server-side implementation of fetch

--- server/fetch.ts
+++ server/fetch.ts
@@ -3,21 +3,20 @@ import { FetchOptions, FetchResponse } from "fetch-types";
 import { peek, Redacted } from "redact";
 import node_fetch from "node-fetch";
 
-function fetch(url: string, options?: FetchOptions) {
-	return node_fetch(url, options).then(response => response.text().then(text => {
-		const headers: { [name: string]: string } = {};
-		response.headers.forEach((value, name) => headers[name] = value);
-		const result: FetchResponse = {
-			type: response.type,
-			url: response.url,
-			status: response.status,
-			ok: response.ok,
-			statusText: response.statusText,
-			text,
-			headers
-		}
-		return result;
-	}));
+async function fetch(url: string, options?: FetchOptions) {
+	const response = await node_fetch(url, options);
+	const headers: { [name: string]: string } = {};
+	response.headers.forEach((value, name) => headers[name] = value);
+	const result: FetchResponse = {
+		type: response.type,
+		url: response.url,
+		status: response.status,
+		ok: response.ok,
+		statusText: response.statusText,
+		text: await response.text(),
+		headers
+	}
+	return result;
 }
 
 export function fromClient(url: string, options?: FetchOptions) : Promise<FetchResponse> {
