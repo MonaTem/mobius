f067890679255be2295a8e2f8c6a8d14f9af0dc3
Ryan Petrich <rpetrich@gmail.com>
2017-09-30 23:39:14 -0400

    Remove warnings for __generator and __awaiter

--- host/client-compiler.ts
+++ host/client-compiler.ts
@@ -127,7 +127,7 @@ function fixTypeScriptExtendsWarning() {
 					const left = node.left;
 					if (node.operator == "||" && left.type == "LogicalExpression" && left.operator == "&&" && left.left.type == "ThisExpression") {
 						const right = left.right;
-						if (right.type == "MemberExpression" && right.object.type == "ThisExpression" && right.property.type == "Identifier" && right.property.name == "__extends") {
+						if (right.type == "MemberExpression" && right.object.type == "ThisExpression" && right.property.type == "Identifier" && /^__/.test(right.property.name)) {
 							path.replaceWith(node.right);
 						}
 					}
