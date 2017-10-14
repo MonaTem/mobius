a7f37f0fc1839ffe2ae25b499e7b149d31ad07a2
Ryan Petrich <rpetrich@gmail.com>
2017-10-14 18:47:07 -0400

    Have the deterministic for...in/Object.keys rewriting iterate through index keys in order first before proceeding to regular string keys (now uses helper functions)

--- host/rewriteForInStatements.ts
+++ host/rewriteForInStatements.ts
@@ -1,8 +1,11 @@
 import { NodePath } from "babel-traverse";
-import { CallExpression, ForInStatement, Identifier, LabeledStatement, VariableDeclarator } from "babel-types";
+import { CallExpression, ForInStatement, Identifier, LabeledStatement, Program, VariableDeclarator } from "babel-types";
 import * as types from "babel-types";
+const template = require("babel-template");
 
 export default function() {
+	let requiresSortHelper = false;
+	let requiresForInHelper = false;
 	return {
 		visitor: {
 			// Rewrite for (... in ...) into the equivalent source that iterates in a well-defined order
@@ -18,8 +21,6 @@ export default function() {
 						}
 					}
 					const node = path.node;
-					const rightIdentifier = path.scope.generateUidIdentifier("right");
-					const keyIdentifier = path.scope.generateUidIdentifier("key");
 					const keysIdentifier = path.scope.generateUidIdentifier("keys");
 					const iIdentifier = path.scope.generateUidIdentifier("i");
 					const keysSubIExpression = types.memberExpression(keysIdentifier, iIdentifier, true);
@@ -29,32 +30,61 @@ export default function() {
 					} else {
 						body.unshift(types.expressionStatement(types.assignmentExpression("=", node.left, keysSubIExpression)));
 					}
-					path.replaceWith(types.blockStatement([
+					path.replaceWith(types.forStatement(
 						types.variableDeclaration("var", [
-							types.variableDeclarator(rightIdentifier, node.right),
-							types.variableDeclarator(keyIdentifier),
-							types.variableDeclarator(keysIdentifier, types.arrayExpression([])),
+							types.variableDeclarator(keysIdentifier, types.callExpression(types.identifier("__enumerate_props"), [node.right])),
 							types.variableDeclarator(iIdentifier, types.numericLiteral(0)),
 						]),
-						types.forInStatement(keyIdentifier, rightIdentifier, types.expressionStatement(types.callExpression(types.memberExpression(keysIdentifier, types.identifier("push")), [keyIdentifier]))),
-						types.forStatement(types.callExpression(types.memberExpression(keysIdentifier, types.identifier("sort")), []), types.binaryExpression("<", iIdentifier, types.memberExpression(keysIdentifier, types.identifier("length"))), types.unaryExpression("++", iIdentifier), types.blockStatement(body)),
-					]));
+						types.binaryExpression("<", iIdentifier, types.memberExpression(keysIdentifier, types.identifier("length"))),
+						types.unaryExpression("++", iIdentifier),
+						types.blockStatement(body))
+					);
 					path.addComment("leading", "Deterministic for (... in ...)");
 					path.skip();
+					requiresForInHelper = true;
 				}
 			},
-			// Rewrite Object.keys(...) into Object.keys(...).sort()
+			// Rewrite Object.keys(...) into Object.keys(...).sort(__sort_keys)
 			CallExpression: {
 				exit(path: NodePath<CallExpression>) {
 					const node = path.node;
 					const callee = node.callee;
 					if (callee.type == "MemberExpression" && callee.object.type == "Identifier" && callee.object.name == "Object") {
 						if (callee.property.type == "Identifier" && callee.property.name == "keys") {
-							path.replaceWith(types.callExpression(types.memberExpression(node, types.identifier("sort")), []));
+							path.replaceWith(types.callExpression(types.memberExpression(node, types.identifier("sort")), [types.identifier("__sort_keys")]));
 							path.addComment("leading", "Deterministic Object.keys(...)");
 							path.skip();
+							requiresSortHelper = true;
 						}
 					}
+				}
+			},
+			Program: {
+				exit(path: NodePath<Program>) {
+					const body = path.get("body.0");
+					if (requiresForInHelper) {
+						body.insertBefore(template(`function __enumerate_props(o) {
+							var result = [];
+							for (var i in o) {
+								result.push(i);
+							}
+							return result.sort(__sort_keys);
+						}`)());
+						requiresSortHelper = true;
+					}
+					if (requiresSortHelper) {
+						body.insertBefore(template(`var __is_numeric = /^\\d+$/; function __sort_keys(a, b) {
+							var a_numeric = __is_numeric.test(a), b_numeric = __is_numeric.test(b);
+							if (a_numeric) {
+								return b_numeric ? a - b : -1;
+							} else if (b_numeric) {
+								return 1;
+							} else {
+								return a < b ? -1 : a > b ? 1 : 0;
+							}
+						}`)());
+					}
+					path.stop();
 				}
 			}
 		}
