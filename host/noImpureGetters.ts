de1a370f6353411f943c218224958f4ac6e12795
Ryan Petrich <rpetrich@gmail.com>
2017-10-20 14:51:08 -0400

    Allow potential non-determinism produced by an impure getter to be ignored by an ignore_nondeterminism label

--- host/noImpureGetters.ts
+++ host/noImpureGetters.ts
@@ -1,9 +1,15 @@
 import { NodePath } from "babel-traverse";
-import { ClassMethod, ObjectMethod } from "babel-types";
+import { ClassMethod, LabeledStatement, ObjectMethod } from "babel-types";
 import { pureBabylon as pure } from "side-effects-safe";
 
 function verifyPurityOfGetters(path: NodePath<ClassMethod> | NodePath<ObjectMethod>) {
 	if (path.node.kind === "get" && !pure(path.node.body, { pureMembers: /./ })) {
+		let ancestor: NodePath = path;
+		while (ancestor = ancestor.parentPath) {
+			if (ancestor.isLabeledStatement() && (ancestor.node as LabeledStatement).label.name === "ignore_nondeterminism") {
+				return;
+			}
+		}
 		throw path.buildCodeFrameError("Impure getter methods may introduce non-determinism as a result of optimization!");
 	}
 }
